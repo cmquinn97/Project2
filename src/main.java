8946e0e2fb787ac6ef6ea75ea6c04fb2df6979a4
Chris Quinn <cmquinn976@gmail.com>
2018-02-25 15:35:04 -0700

    Workinggit status

--- src/main.java
+++ src/main.java
@@ -1,16 +1,42 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
 public class main {
+	static boolean one = false;
+	static boolean two = false;
+	static boolean three = false;
+	static int idNum = 0;
 
 	public static void main(String[] args) {
-		
-		Random r = new Random();
-
 		Lists queue = new Lists();
-		for(int i = 0; i<10; i++){
+		three = true;
+		if(one){
+			populateQueue(20, queue);
+		}
+		if(two){
+			populateQueue(5, queue);
+		}
+		if(three){
+			populateQueue(10, queue);
+		}
+		//If first three in queue are all same gender, they all enter
+		if(queue.queue.get(0).gender == queue.queue.get(1).gender && queue.queue.get(1).gender == queue.queue.get(2).gender){
+			while(queue.bRoom.size()<2){
+				queue.queue.get(0).UseFacilities(queue.queue, queue.bRoom);
+			}
+		}
+		queue.queue.get(0).UseFacilities(queue.queue, queue.bRoom);
+		bRoomProcess(queue);
 			
+	}
+	
+	public static void populateQueue(int amount, Lists queue){
+		Random r = new Random();
+		
+		for(int i = 0; i<amount; i++){
 			int gender; float chance = r.nextFloat(); int time = r.nextInt(7-3) + 3;
 			
 			if (chance <= .60f){
@@ -19,27 +45,68 @@ public class main {
 				gender = 1;
 			}
 			
-			OnePerson person = new OnePerson(i, gender, time);
+			OnePerson person = new OnePerson(idNum, gender, time);
 			person.Arrive(queue.queue);
+			idNum++;
 		}
-			queue.queue.get(0).UseFacilities(queue.queue, queue.bRoom);
-			bRoomProcess(queue);
-			
-		
 	}
+	
+
 
 	public static void bRoomProcess(Lists queue){
-		while(queue.bRoom.size()>0){
-			int remove = 0;
-			int lowTime = 100;
-			for (OnePerson person : queue.bRoom){
-				if(person.time < lowTime ){
-					lowTime = person.time;
-					remove = queue.bRoom.indexOf(person);
+		List<OnePerson> toRemove = new ArrayList<OnePerson>();
+		int populatedTimes = 0;
+		
+		while(OnePerson.departure < 20){
+			for(OnePerson p : queue.bRoom){
+				if(p.time == OnePerson.timeTotal){
+					toRemove.add(p);
+				}
+			}
+			if(toRemove.isEmpty()){
+				OnePerson.timeTotal +=1;
+			}
+			for(OnePerson remove : toRemove){
+				remove.Depart(queue.bRoom, queue.queue);
+			}
+			if(!toRemove.isEmpty()){
+				toRemove.removeAll(toRemove);
+				
+			}
+			if(queue.bRoom.size() == 0){
+				if(OnePerson.departure == 20){
+					break;
+				}
+				//Checks for the situation where three would be able to enter
+				if(queue.queue.get(0).gender == queue.queue.get(1).gender && queue.queue.get(1).gender == queue.queue.get(2).gender){
+					while(queue.bRoom.size()<2){
+						queue.queue.get(0).UseFacilities(queue.queue, queue.bRoom);
+					}
+				}
+				try{
+					Lists.NextUser(queue.queue, queue.bRoom).UseFacilities(queue.queue, queue.bRoom);
+				}catch(NullPointerException e){
+					continue;
+					
 				}
 				
 			}
-			queue.bRoom.get(remove).Depart(queue.bRoom, queue.queue, remove);
+			if(queue.bRoom.size()<2){
+				if(!queue.queue.isEmpty()){
+					if(Lists.NextUser(queue.queue, queue.bRoom).gender == queue.bRoom.get(0).gender){
+						Lists.NextUser(queue.queue, queue.bRoom).UseFacilities(queue.queue, queue.bRoom);
+					}
+				}
+			}
+			if(two == true && OnePerson.timeTotal == 10 && populatedTimes == 0 || two == true && OnePerson.timeTotal == 20 && populatedTimes == 1 || two == true && OnePerson.timeTotal == 30 && populatedTimes == 2){
+				populateQueue(5, queue);
+				populatedTimes ++;
+			}
+			if(three == true && OnePerson.timeTotal == 10 && populatedTimes == 0){
+				populateQueue(10, queue);
+				populatedTimes ++;
+			}
+			
 		}
 	}
 }
@@ -47,7 +114,7 @@ public class main {
 
 class OnePerson{
 	int id, gender, time;
-	static int timeTotal;
+	static int timeTotal, departure = 0;
 	String genderChar;
 	
 	
@@ -80,34 +147,25 @@ class OnePerson{
 		try{
 			if(!bRoom.isEmpty()&& bRoom.size() < 2){
 				Lists.NextUser(queue, bRoom).UseFacilities(queue, bRoom);
-			}
-				
-			
-		}catch(IndexOutOfBoundsException e){
-			
-		}	
+			}	
+		}catch(IndexOutOfBoundsException e){}	
 	}
 	
 	
-	public void Depart(ArrayList<OnePerson> bRoom,ArrayList<OnePerson> queue, int remove ){
-		bRoom.remove(remove);
-		timeTotal = timeTotal + (this.time - timeTotal);
-		
-		System.out.println("Time: "+ timeTotal + "; Person " + this.id + genderChar + " exits.");
-		if(!queue.isEmpty()){
-			if(!bRoom.isEmpty()){
-				Lists.NextUser(queue, bRoom).UseFacilities(queue, bRoom);
-			}
-		}
+	public void Depart(ArrayList<OnePerson> bRoom,ArrayList<OnePerson> queue){
+		bRoom.remove(this);
+		departure++;
+		System.out.println("Time: "+ timeTotal + "; Person " + this.id + genderChar + " exits." + " Departure = " + departure);
 		
 	}
 }
  
 class Lists{
+	
 	ArrayList<OnePerson> queue = new ArrayList<OnePerson>();
 	ArrayList<OnePerson> bRoom = new ArrayList<OnePerson>();
 	
-public static OnePerson NextUser(ArrayList<OnePerson> queue, ArrayList<OnePerson> bRoom){
+	public static OnePerson NextUser(ArrayList<OnePerson> queue, ArrayList<OnePerson> bRoom){
 		OnePerson returnPerson = null;
 			if(bRoom.isEmpty()){
 				returnPerson = queue.get(0);
